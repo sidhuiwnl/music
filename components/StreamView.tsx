3c548382dab710514fd2ef759ab369cbf3e4bec8
Sidharth <sidharthinfernal@gmail.com>
2024-09-08 21:44:48 +0530

    added playing concurrent videos

--- components/StreamView.tsx
+++ components/StreamView.tsx
@@ -4,51 +4,74 @@ import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
 import { Card, CardContent } from "./ui/card";
 import { YT_REGEX } from "@/lib/utils";
 import { Button } from "./ui/button";
-import {youtubeStream,getVideos } from "@/lib/action";
+import { youtubeStream, getVideos } from "@/lib/action";
 import Image from "next/image";
-
-
+import { X, ChevronUp } from "lucide-react";
+import YouTubePlayer from 'youtube-player'
 
 interface VideoMetaData {
   userId: string;
   title: string;
   thumbnail: string;
-  youtubeLink  : string;
+  youtubeLink: string;
 }
-
-
-
-
 
 export default function StreamView({ userId }: { userId: string }) {
   const [youtubeLink, setYoutubeLink] = useState<string | null>(null);
   const [videoMetaDatas, setVideoMetaDatas] = useState<VideoMetaData[]>([]);
+  const [currentlyPlaying,setCurrentlyPlaying] = useState<VideoMetaData | null>(null);
+  const playerRef = useRef<any>(null);
 
-  
-   useEffect(() =>{
-    const userVideoDetails = async() =>{
-      const datas = await getVideos({id : userId})
-      
-      setVideoMetaDatas(datas.streams)
+  useEffect(() => {
+    const userVideoDetails = async () => {
+      const datas = await getVideos({ id: userId });
+
+      setVideoMetaDatas(datas.streams);
+    };
+
+    userVideoDetails();
+  }, [userId]);
+
+  useEffect(() =>{
+    if(currentlyPlaying){
+      const player = YouTubePlayer(playerRef.current);
+      player.loadVideoById(currentlyPlaying.youtubeLink);
+      player.playVideo();
+
+      const eventHandler = (event : {data : number}) =>{
+        if(event.data === 0){
+          playNext();
+        }
+      }
+      player.on('stateChange', eventHandler)
+
+      return () => {
+        player.destroy()
     }
 
-    userVideoDetails()
-   },[userId])
+    }
+  },[currentlyPlaying])
 
   async function addtoRedis() {
     if (youtubeLink) {
       try {
         const datas = await youtubeStream({ id: userId, link: youtubeLink });
-        
-        const parsedData = datas.streams.map((data) => {
-          return JSON.parse(data)
-        })
 
-        setVideoMetaDatas(parsedData)
+        const parsedData = datas.streams.map((data) => {
+          return JSON.parse(data);
+        });
+
+        setVideoMetaDatas(parsedData);
+        setYoutubeLink(null);
+
+        if(!currentlyPlaying && parsedData.length > 0){
+          setCurrentlyPlaying(parsedData[0]);
+          setVideoMetaDatas(videoMetaDatas.slice(1));
+        }
         console.log("YouTube link added to queue");
       } catch (error) {
         console.error("Error adding to queue:", error);
@@ -56,11 +79,28 @@ export default function StreamView({ userId }: { userId: string }) {
     }
   }
 
+  function playNext() {
+    if(videoMetaDatas.length > 0){
+      try {
+        const nextVideo = videoMetaDatas[0];
+        setCurrentlyPlaying(nextVideo);
+        setVideoMetaDatas(videoMetaDatas.slice(1));
+      } catch (error) {
+        console.error(error)
+      }
+    }else{
+      setCurrentlyPlaying(null)
+    }
+    
+  }
 
   return (
     <div className="flex p-4">
       <div className="flex flex-col w-[435px] p-4 space-y-2">
-        <Label htmlFor="youtube-link" className="font-bold font-mono mb-5 text-xl">
+        <Label
+          htmlFor="youtube-link"
+          className="font-bold font-mono mb-5 text-xl"
+        >
           Add Your Favourite Song to be Played
         </Label>
         <Input
@@ -70,7 +110,9 @@ export default function StreamView({ userId }: { userId: string }) {
             videoId ? setYoutubeLink(videoId[1]) : setYoutubeLink(null);
           }}
         />
-        <Button onClick={addtoRedis} className="font-mono">Add to Queue</Button>
+        <Button onClick={addtoRedis} className="font-mono">
+          Add to Queue
+        </Button>
         <div>
           {youtubeLink && (
             <Card className="bg-black w-[400px] rounded-lg p-3 mt-5">
@@ -81,10 +123,13 @@ export default function StreamView({ userId }: { userId: string }) {
       </div>
       <div className="p-4 space-y-2">
         <div>
-          <Label className="font-bold font-mono mb-5 text-xl">Upcoming Songs</Label>
+          <Label className="font-bold font-mono mb-5 text-xl">
+            Upcoming Songs
+          </Label>
         </div>
-        <Card className="flex flex-col space-y-4 p-4 min-w-[800px] min-h-[100px] bg-gray-900 border-gray-800 text-white">
+
         {videoMetaDatas?.map((videoMetaData, index) => (
+          <Card className="flex flex-col space-y-4 p-4 min-w-[800px] min-h-[100px] bg-gray-900 border-gray-800 text-white">
             <CardContent key={index} className="flex items-center space-x-4">
               <Image
                 src={videoMetaData.thumbnail}
@@ -93,13 +138,33 @@ export default function StreamView({ userId }: { userId: string }) {
                 height={100}
                 className="rounded-lg"
               />
-              <h1 className="text-lg font-semibold">{videoMetaData.title}</h1>
+              <div className="flex flex-col space-y-2 items-start">
+                <h1 className="text-lg font-semibold">{videoMetaData.title}</h1>
+                <div className="flex flex-row space-x-2">
+                  <Button className="w-14" variant="secondary">
+                    <X />
+                  </Button>
+                  <Button className="w-14" variant="secondary">
+                    <ChevronUp />
+                  </Button>
+                </div>
+              </div>
             </CardContent>
-          ))}
+          </Card>
+        ))}
+      </div>
+      <div className="p-4 space-y-2">
+        <Label className="font-bold font-mono mb-5 text-xl">Currently Playing</Label>
+        <Card className=" min-w-[400px] min-h-[200px]  bg-gray-900 border-gray-800 text-white">
+          <div ref={playerRef}>
 
-          
+          </div>
         </Card>
       </div>
+      
     </div>
   );
 }
+
+
+
