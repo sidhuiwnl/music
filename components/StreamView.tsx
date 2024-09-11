e0e23f538adf8b5183b3687ee429152447d71ab1
Sidharth <sidharthinfernal@gmail.com>
2024-09-11 23:04:15 +0530

    from start

--- components/StreamView.tsx
+++ components/StreamView.tsx
@@ -4,105 +4,111 @@ import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { Card, CardContent } from "./ui/card";
 import { YT_REGEX } from "@/lib/utils";
 import { Button } from "./ui/button";
-import { youtubeStream, getVideos, deleteVideoFromQueue } from "@/lib/action";
+import { addToRedis,getStreams } from "@/lib/action";
+import client from "@/lib/redis";
 import Image from "next/image";
 import { X, ChevronUp } from "lucide-react";
 import YouTubePlayer from "youtube-player";
 
-interface VideoMetaData {
-  userId: string;
-  title: string;
-  thumbnail: string;
-  youtubeLink: string;
+// interface VideoMetaData {
+//   userId: string;
+//   title: string;
+//   thumbnail: string;
+//   youtubeLink: string;
+// }
+
+interface YouTubePlayerInstance {
+  loadVideoById: (videoId: string) => void;
+  playVideo: () => void;
+  destroy: () => void;
+  on: (event: string, listener: (event: { data: number }) => void) => void;
 }
 
 export default function StreamView({ userId }: { userId: string }) {
   const [youtubeLink, setYoutubeLink] = useState<string | null>(null);
-  const [videoMetaDatas, setVideoMetaDatas] = useState<VideoMetaData[]>([]);
-  const [currentlyPlaying, setCurrentlyPlaying] =
-    useState<VideoMetaData | null>(null);
-  const playerRef = useRef<any>(null);
+  const [streams,setStreams] = useState("");
+  // const [videoMetaDatas, setVideoMetaDatas] = useState<VideoMetaData[]>([]);
+  // const [currentlyPlaying, setCurrentlyPlaying] = useState<VideoMetaData | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  // const playerRef = useRef<HTMLDivElement>(null);
+  // const playerInstanceRef = useRef<YouTubePlayerInstance | null>(null);
 
-  useEffect(() => {
-    const userVideoDetails = async () => {
-      const datas = await getVideos({ id: userId });
-
-      setVideoMetaDatas(datas.streams);
-    };
-
-    userVideoDetails();
-  }, [userId]);
-
-  useEffect(() => {
-    if (currentlyPlaying) {
-      const player = YouTubePlayer(playerRef.current);
-      player.loadVideoById(currentlyPlaying.youtubeLink);
-      player.playVideo();
-
-      // Event listener for when the video ends
-      const handleVideoStateChange = async (event: { data: number }) => {
-        if (event.data === 0) {
-          await deleteVideoFromQueue({id : userId,link : currentlyPlaying.youtubeLink})
-          playNext();
-        }
-      };
-
-      player.on("stateChange", handleVideoStateChange);
-
-      return () => {
-        player.destroy();
-      };
+  
+  async function addtolocalRedis(){
+    if(!youtubeLink){
+      throw new Error("add some videos to view")
     }
-  }, [currentlyPlaying]);
+    const result = await addToRedis({id : userId,youtubeLink : youtubeLink});
 
-  async function addtoRedis() {
-    if (youtubeLink) {
-      try {
-        const datas = await youtubeStream({ id: userId, link: youtubeLink });
+    console.log(result)
 
-        const parsedData = datas.streams.map((data) => {
-          return JSON.parse(data);
-        });
-
-        setVideoMetaDatas(parsedData);
-        setYoutubeLink(null);
-
-        if (!currentlyPlaying && parsedData.length > 0) {
-          setCurrentlyPlaying(parsedData[0]);
-          setVideoMetaDatas(videoMetaDatas.slice(1));
-        }
-        console.log("YouTube link added to queue");
-      } catch (error) {
-        console.error("Error adding to queue:", error);
-      }
-    }
+    
   }
 
-  function playNext() {
-    if (videoMetaDatas.length > 0) {
-      try {
-        const nextVideo = videoMetaDatas[0];
-        setCurrentlyPlaying(nextVideo);
-        setVideoMetaDatas(videoMetaDatas.slice(1));
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      setCurrentlyPlaying(null);
-    }
+  async function streamsget(){
+    const response = await getStreams({id : userId});
+    console.log(response)
   }
+
+  
+
+  // const playNext = useCallback(() => {
+  //   if (videoMetaDatas.length > 0) {
+  //     const nextVideo = videoMetaDatas[0];
+  //     setCurrentlyPlaying(nextVideo);
+  //     setVideoMetaDatas(prevVideos => prevVideos.slice(1));
+  //   } else {
+  //     setCurrentlyPlaying(null);
+  //   }
+  // }, [videoMetaDatas]);
+
+  // useEffect(() => {
+  //   if (currentlyPlaying && playerRef.current) {
+  //     if (!playerInstanceRef.current) {
+  //       playerInstanceRef.current = YouTubePlayer(playerRef.current) as YouTubePlayerInstance;
+  //     }
+
+  //     const player = playerInstanceRef.current;
+  //     player.loadVideoById(currentlyPlaying.youtubeLink);
+  //     player.playVideo();
+
+  //     const handleVideoStateChange = (event: { data: number }) => {
+  //       if (event.data === 0) { // Video ended
+  //         playNext();
+  //       }
+  //     };
+
+  //     player.on("stateChange", handleVideoStateChange);
+
+  //     return () => {
+  //       player.destroy();
+  //       playerInstanceRef.current = null;
+  //     };
+  //   }
+  // }, [currentlyPlaying, playNext]);
+
+ 
+
+  // const handleDeleteFromQueue = useCallback(async (video: VideoMetaData) => {
+  //   try {
+  //     await deleteVideoFromQueue({ id: userId, link: video.youtubeLink });
+  //     setVideoMetaDatas(prevVideos =>
+  //       prevVideos.filter(prevVideo => prevVideo.youtubeLink !== video.youtubeLink)
+  //     );
+  //   } catch (error) {
+  //     console.error("Error deleting video:", error);
+  //     setError("Failed to delete video from queue. Please try again.");
+  //   }
+  // }, [userId]);
 
   return (
-    <div className="flex p-4">
-      <div className="flex flex-col w-[435px] p-4 space-y-2">
-        <Label
-          htmlFor="youtube-link"
-          className="font-bold font-mono mb-5 text-xl"
-        >
+    <div className="flex flex-col md:flex-row p-4 space-y-4 md:space-y-0 md:space-x-4">
+      <div className="flex flex-col w-full md:w-[435px] p-4 space-y-2">
+        <Label htmlFor="youtube-link" className="font-bold font-mono mb-5 text-xl">
           Add Your Favourite Song to be Played
         </Label>
         <Input
@@ -112,65 +118,79 @@ export default function StreamView({ userId }: { userId: string }) {
             videoId ? setYoutubeLink(videoId[1]) : setYoutubeLink(null);
           }}
         />
-        <Button onClick={addtoRedis} className="font-mono">
+        <Button  onClick={addtolocalRedis} className="font-mono">
           Add to Queue
         </Button>
-        <div>
-          {youtubeLink && (
-            <Card className="bg-black w-[400px] rounded-lg p-3 mt-5">
-              <LiteYouTubeEmbed id={youtubeLink} title={youtubeLink} />
-            </Card>
-          )}
-        </div>
-      </div>
-      <div className="p-4 space-y-2">
-        <div>
-          <Label className="font-bold font-mono mb-5 text-xl">
-            Upcoming Songs
-          </Label>
-        </div>
-
-        
-          <Card className="flex flex-col space-y-4 p-4 min-w-[800px] min-h-[100px] bg-gray-900 border-gray-800 text-white">
-          {videoMetaDatas.length > 0 ? videoMetaDatas?.map((videoMetaData, index) => (
-            <CardContent key={index} className="flex items-center space-x-4">
-              <Image
-                src={videoMetaData.thumbnail}
-                alt={videoMetaData.title}
-                width={100}
-                height={100}
-                className="rounded-lg"
-              />
-              <div className="flex flex-col space-y-2 items-start">
-                <h1 className="text-lg font-semibold">{videoMetaData.title}</h1>
-                <div className="flex flex-row space-x-2">
-                  <Button className="w-14" variant="secondary">
-                    <X />
-                  </Button>
-                  <Button className="w-14" variant="secondary">
-                    <ChevronUp />
-                  </Button>
-                </div>
-              </div>
-            </CardContent>
-             )) : <CardContent><p className="text-white">Nothing to play</p></CardContent>}
+        <Button  onClick={streamsget} className="font-mono">
+          streams
+        </Button>
+        {youtubeLink && (
+          <Card className="bg-black w-full rounded-lg p-3 mt-5">
+            <LiteYouTubeEmbed id={youtubeLink} title={youtubeLink} />
           </Card>
-       
+        )}
       </div>
-      <div className="p-4 space-y-2">
+
+      {/* <div className="flex flex-col w-full md:w-auto p-4 space-y-2">
+        <Label className="font-bold font-mono mb-5 text-xl">
+          Upcoming Songs
+        </Label>
+        <Card className="flex flex-col space-y-4 p-4 w-full md:min-w-[400px] min-h-[100px] bg-gray-900 border-gray-800 text-white">
+          {videoMetaDatas.length > 0 ? (
+            videoMetaDatas.map((videoMetaData, index) => (
+              videoMetaData && videoMetaData.thumbnail && videoMetaData.title ? (
+                <CardContent key={index} className="flex items-center space-x-4">
+                  <Image
+                    src={videoMetaData.thumbnail}
+                    alt={videoMetaData.title}
+                    width={100}
+                    height={100}
+                    className="rounded-lg"
+                    onError={(e) => {
+                      e.currentTarget.src = "/placeholder-image.jpg"; // Replace with an actual placeholder image path
+                    }}
+                  />
+                  <div className="flex flex-col space-y-2 items-start">
+                    <h2 className="text-lg font-semibold">{videoMetaData.title}</h2>
+                    <div className="flex flex-row space-x-2">
+                      <Button 
+                        onClick={() => handleDeleteFromQueue(videoMetaData)}
+                        className="w-14"
+                        variant="secondary"
+                        aria-label="Remove from queue"
+                      >
+                        <X />
+                      </Button>
+                      <Button className="w-14" variant="secondary" aria-label="Move up in queue">
+                        <ChevronUp />
+                      </Button>
+                    </div>
+                  </div>
+                </CardContent>
+              ) : null
+            ))
+          ) : (
+            <CardContent><p className="text-white">No upcoming songs in the queue</p></CardContent>
+          )}
+        </Card>
+      </div>
+      <div className="flex flex-col w-full md:w-auto p-4 space-y-2">
         <Label className="font-bold font-mono mb-5 text-xl">
           Currently Playing
         </Label>
-        <Card className=" min-w-[400px] min-h-[200px] rounded  bg-gray-900 border-gray-800 text-white">
+        <Card className="w-full md:min-w-[400px] min-h-[200px] rounded bg-gray-900 border-gray-800 text-white">
           <CardContent>
-            <div className="" ref={playerRef}></div>
+            <div ref={playerRef}></div>
           </CardContent>
-          
         </Card>
-        <Button
-        onClick={playNext}
-        >Play Next</Button>
-      </div>
+        <Button onClick={playNext}>Play Next</Button>
+      </div> */}
+
+      {error && (
+        <div className="text-red-500 mt-2">
+          {error}
+        </div>
+      )}
     </div>
   );
 }
--- lib/action.ts
+++ lib/action.ts
@@ -1,65 +1,108 @@
+"use server"
+
+import client from "./redis";
+import ytdl from 'ytdl-core';
+
 
 interface StreamResponse {
     msg: string;
     streams: string[];
 }
 
-
-
-
-export async function youtubeStream({id,link} : {id : string , link : string}){
-    const response  = await fetch("/api/streams",{
-        method : "POST",
-        headers : {
-            'Content-Type' : 'application/json'
-        },
-        body : JSON.stringify({
-            youtubeLink : link,
-            userId : id
-        })
-    })
-
-    if(!response.ok){
-        throw new Error('Failed to submit the YouTube link');
+export async function addToRedis({ id, youtubeLink }: { id: string, youtubeLink: string }) {
+    if (!id) {
+        throw new Error("Please sign-in for adding to playlist");
     }
 
-    const responseData: StreamResponse = await response.json();
-    return responseData;
-}
+    const info = await ytdl.getInfo(youtubeLink);
+    const title  = info.videoDetails.title;
+    const thumbnail = info.videoDetails.thumbnail.thumbnails[0].url;
 
-export async function getVideos( { id } : { id : string}){
-    const response = await fetch("/api/streaminfo",{
-        method : "POST",
-        headers : {
-            'Content-Type' : 'application/json'
-        },
-        body : JSON.stringify({
-            userId : id
-        })
-    })
+    const existingLinks = await client.lRange(`streams:${id}`, 0, -1);
 
-    if(!response.ok){
-        throw new Error('Failed to submit the get streams');
+    if (existingLinks.includes(youtubeLink)) {
+        throw new Error("This YouTube link has already been added.");
     }
 
-    return response.json()
+    
+    await client.lPush(`streams:${id}`, JSON.stringify({
+        youtubeLink,title,thumbnail
+    }));
 }
 
 
-export async function deleteVideoFromQueue({id,link} : {id :string,link : string}){
-    const response = await fetch("/api/deleteVideo",{
-        method : "DELETE",
-        headers: { 'Content-Type': 'application/json' },
-        body : JSON.stringify({
-            userId : id,
-            youtubeLink  : link
-        })
-    })
 
-    if(!response.ok){
-        throw new Error('Failed to submit the get streams');
+export async function getStreams({ id }: { id: string }) {
+   
+    const allKeys = await client.keys("streams:*");
+
+   
+    const userKey = `streams:${id}`;
+    
+    if (allKeys.includes(userKey)) {
+        
+        const streams = await client.lRange(userKey, 0, -1);
+        
+        
+        return streams;
+    } else {
+        throw new Error("User not found or no streams available.");
     }
-
-    return response.json()
-
 }
+
+// export async function youtubeStream({id,link} : {id : string , link : string}){
+//     const response  = await fetch("/api/streams",{
+//         method : "POST",
+//         headers : {
+//             'Content-Type' : 'application/json'
+//         },
+//         body : JSON.stringify({
+//             youtubeLink : link,
+//             userId : id
+//         })
+//     })
+
+//     if(!response.ok){
+//         throw new Error('Failed to submit the YouTube link');
+//     }
+
+//     const responseData: StreamResponse = await response.json();
+//     return responseData;
+// }
+
+// export async function getVideos( { id } : { id : string}){
+//     const response = await fetch("/api/streaminfo",{
+//         method : "POST",
+//         headers : {
+//             'Content-Type' : 'application/json'
+//         },
+//         body : JSON.stringify({
+//             userId : id
+//         })
+//     })
+
+//     if(!response.ok){
+//         throw new Error('Failed to submit the get streams');
+//     }
+
+//     return response.json()
+// }
+
+
+// export async function deleteVideoFromQueue({id,link} : {id :string,link : string}){
+//     const response = await fetch("/api/deleteVideo",{
+//         method : "DELETE",
+//         headers: { 'Content-Type': 'application/json' },
+//         body : JSON.stringify({
+//             userId : id,
+//             youtubeLink  : link
+//         })
+//     })
+
+//     if(!response.ok){
+//         throw new Error('Failed to submit the get streams');
+//     }
+
+//     return response.json()
+
+// }
